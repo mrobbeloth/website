7a3ae14433a16130d68faac946a4de6487530cf1
Michael Robbeloth <michael@robbeloth.com>
2025-06-08 14:02:43 -0400

    Add menu; update content for new position

--- index.php
+++ index.php
@@ -5,9 +5,19 @@
 <title> Robbeloth Family Website </title>
 <link rel="stylesheet" href="stylesheet.css" />
 <link rel="icon" href="img/R.png" type="image/x-icon">
+
+<nav>
+ <a href="consulting.html"><div>Hire Me</div></a>
+ <a href="https://web.archive.org/web/20250528182834/https://cse.osu.edu/people/robbeloth.1"><div>Work @ OSU</div></a>
+ <a href="https://www.linkedin.com/in/mrobbeloth/"><div>Linked In Profile</div></a>
+ <a href="https://www.twitter.com/mrobbeloth    "><div>Twitter Profile</div></a>
+ <a href="https://github.com/mrobbeloth"><div>Github Profile</div></a>
+</nav>
+
 </head>
 <body>
 
+<br/>
 <h1>Welcome to the Robbeloth Family Website</h1>
 <img src="img/Robbeloth-2671-reduced.jpg" width="600" height="400" alt="The North-Central Ohio based Robbeloth Family"> 
 
@@ -20,15 +30,16 @@ echo "You are using: " . $user_agent_string . '.';
 ?>
 </p>
 
-<p title="Important links">  Dr. Michael Robbeloth is a senior lecturer at OSU in the <a href="https://engineering.osu.edu/">College of Engineering&apos;s</a> <a href="https://cse.osu.edu/">Department of Computer Science and Engineering</a>. Previously, I was an associate professor of <a href="https://cs.mvnu.edu/">Computer Science</a> at 
-Mount Vernon Nazarene University (<a href="https://www.mvnu.edu">MVNU</a>). Dr. Robbeloth is also available to take on professonal speaking engagements or consulting work (see my consulting page). 
+<p title="Important links">  Dr. Michael Robbeloth is an Assistant Professor of Practice in the 
+<a href="https://engineering.osu.edu/">College of Engineering&apos;s</a> <a href="https://eed.osu.edu/">Engineering Education Department </a> with a courtesy
+appointment in the <a href="https://cse.osu.edu/">Department of Computer Science and Engineering</a>. Previously, Dr. Robbeloth was an Associate Professor of <a href="https://cs.mvnu.edu/">Computer Science</a> at 
+Mount Vernon Nazarene University (<a href="https://www.mvnu.edu">MVNU</a>) and worked in industry for companies like <a href="https://www.kodak.com">Eastman Kodak</a>, <a href="https://dsautomation.com/">DSA Automation</a>, and <a href="https://www.pdiarm.com">PDi Communications, Inc.</a>
+Dr. Robbeloth is available to take on professonal speaking engagements or consulting work (see my <a href="consulting.html">consulting</a> page).
 You can learn more about Dr. Robbeloth from following one of the links below: </p>
 <ol>
-    <a href="https://about.me/mrobbeloth">About Dr. Robbeloth</a> <br/>
     <a href="consulting.html">Consulting Information</a> <br/>
-    <a href="https://cse.osu.edu/people/robbeloth.1">Work: OSU</a> <br/>
-    <a href="https://mvnu.edu/faculty/dr-michael-robbeloth/">Work: MVNU </a> <br/>
-    <a href="https://www.linkedin.com/in/mrobbeloth/"> Dr. Robbeloth&apos;s Linked In Profile </a> <br/>
+    <a href="https://cse.osu.edu/people/robbeloth.1">Work @ OSU</a> <br/>
+    <a href="https://www.linkedin.com/in/mrobbeloth/"> Linked In Profile </a> <br/>
     <a href="https://www.twitter.com/mrobbeloth">Twitter</a> <br/>
     <a href="https://github.com/mrobbeloth">Github</a> <br/>
 </ol>
